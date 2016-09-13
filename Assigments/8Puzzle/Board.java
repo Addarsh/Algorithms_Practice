3c254b31019d0c311c761947f8005146ba585cce
Addarsh <addarsh.cherokie@gmail.com>
2016-09-13 15:26:56 -0700

    isSolvable method tested and working

--- Assigments/8Puzzle/Board.java
+++ Assigments/8Puzzle/Board.java
@@ -6,19 +6,23 @@ import edu.princeton.cs.algs4.StdRandom;
 public class Board{
 	int n;
 	int[][] myboard;
-
+	int[] myarr;
+	static final int DEBUG  = 1;
+	
 	//Constructor
 	public Board(int[][] blocks){
 		n = blocks.length;
 		myboard = new int[n][n];
+		myarr = new int[n*n];
 		for(int i =0; i < n; i++){
 			for(int j =0; j < n; j++){
 				myboard[i][j] = blocks[i][j];
+				myarr[n*i+j] = blocks[i][j];
 			}
 		}
 	}
 
-	public int dimension(){
+	public int size(){
 		return n;
 	}
 
@@ -49,7 +53,7 @@ public class Board{
 						col = myboard[i][j] % 3-1;
 						row = myboard[i][j] /3;
 					}
-					distance += col- j + row - i;
+
 				} 
 			}
 		}
@@ -60,6 +64,12 @@ public class Board{
 		return hamming() == 0;
 	}
 
+	public boolean equals(Object y){
+		Board other = (Board) y;
+		if(this.toString() != other.toString()) return false;
+		return true;
+	}
+
 	private void searchForElem(int i,int[] myarr){
 		for(int j = 0; j <n; j++){
 			if(myboard[i][j] != 0){
@@ -67,6 +77,38 @@ public class Board{
 				myarr[i+2] = j;
 				break;
 			}
+		}
+	}
+
+	private void getInversions(int[] arr){
+		int numInversions = 0;
+		for(int i =1; i < myarr.length;i++){
+			int num = myarr[i];
+			if(num == 0) {
+				arr[0] = i/n;
+				continue;
+			}
+			for(int j=0; j < i;j++){
+				if(num < myarr[j]) numInversions++;
+			}
+		} 
+		arr[1] = numInversions;
+	}
+
+	public boolean isSolvable(){
+		int[] arr = new int[2];
+		getInversions(arr);
+		
+		if(n % 2 != 0){
+			//Odd size
+			System.out.printf("%d %d\n",arr[0],arr[1]);
+			if(arr[1] % 2 != 0) return false;
+			return true;	
+		} else{
+			//Even size
+			System.out.printf("%d %d\n",arr[0],arr[1]);
+			if((arr[0] + arr[1]) % 2 == 0) return false;
+			return true;
 		}
 	}
 
@@ -115,27 +157,34 @@ public class Board{
 	}
 
 	private static void create2Dmatrix(int[][] blocks){
-		int[] myarr = new int[blocks.length*blocks.length];
-		for(int i =0; i < myarr.length; i++){
-			myarr[i] = i;
+		int[] myarr;
+		if(DEBUG == 0){
+			myarr = new int[blocks.length*blocks.length];
+			for(int i =0; i < myarr.length; i++){
+				myarr[i] = i;
+			}
+			//Shuffle array
+			shuffle(myarr);
+		}else{
+			myarr = new int[]{1,2,3,4,5,6,0,8,9,10,7,11,13,14,15,12};	
+			//myarr = new int[]{0,1,3,4,2,5,7,8,6};	
 		}
-		//Shuffle array
-		shuffle(myarr);
 		int totalindex = 0;
 		for(int i =0; i < blocks.length;i++){	
 			for(int j =0; j < blocks.length;j++){
 				blocks[i][j] = myarr[totalindex];
-				totalindex += 1;	
+				totalindex++;	
 			}	
 		}
 	}
 
 	public static void main(String args[]){
-		int size = 3;
+		int size = 4;
 		int[][] blocks = new int[size][size];
 		create2Dmatrix(blocks);
 		Board myboard = new Board(blocks);		
 		System.out.printf("%s",myboard.toString());
+		System.out.println(myboard.isSolvable());	
 	}
 }
 
